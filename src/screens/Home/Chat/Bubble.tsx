cd6a4bd65e6732237813c33768c066d3bde9d7cc
gustavo867 <tartatavo3@gmail.com>
2021-02-02 20:49:40 -0300

    :wrench: auth changes

--- src/screens/Home/Chat/Bubble.tsx
+++ src/screens/Home/Chat/Bubble.tsx
@@ -4,14 +4,51 @@ import * as S from './styles';
 interface Bubble {
   isSendByMe: boolean;
   text: string;
+  photo: string;
 }
 
-const Bubble: React.FC<Bubble> = ({ isSendByMe, text }) => {
-  return (
-    <S.BubbleContainer isSendByMe={isSendByMe}>
-      <S.BubbleText isSendByMe={isSendByMe}>{text}</S.BubbleText>
-    </S.BubbleContainer>
-  );
+const Bubble: React.FC<Bubble> = ({ isSendByMe, text, photo }) => {
+  switch (isSendByMe) {
+    case true:
+      return (
+        <S.BubbleMainContainer isSendByMe={isSendByMe}>
+          <S.BubbleContainer isSendByMe={isSendByMe}>
+            <S.BubbleText isSendByMe={isSendByMe}>{text}</S.BubbleText>
+          </S.BubbleContainer>
+          <S.BubblePhoto
+            source={{
+              uri: photo,
+            }}
+          />
+        </S.BubbleMainContainer>
+      );
+    case false:
+      return (
+        <S.BubbleMainContainer isSendByMe={isSendByMe}>
+          <S.BubblePhoto
+            source={{
+              uri: photo,
+            }}
+          />
+          <S.BubbleContainer isSendByMe={isSendByMe}>
+            <S.BubbleText isSendByMe={isSendByMe}>{text}</S.BubbleText>
+          </S.BubbleContainer>
+        </S.BubbleMainContainer>
+      );
+    default:
+      return (
+        <S.BubbleMainContainer isSendByMe={isSendByMe}>
+          <S.BubblePhoto
+            source={{
+              uri: photo,
+            }}
+          />
+          <S.BubbleContainer isSendByMe={isSendByMe}>
+            <S.BubbleText isSendByMe={isSendByMe}>{text}</S.BubbleText>
+          </S.BubbleContainer>
+        </S.BubbleMainContainer>
+      );
+  }
 };
 
 export default Bubble;
